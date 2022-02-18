ac803be91a034e06d974b273bdeb7a109f6ca31b
Diego Hernández <dahernandezsilvestre@gmail.com>
2022-02-17 19:44:43 -0600

    Evaluación de postfix

--- HT2/src/PostfixCalc.java
+++ HT2/src/PostfixCalc.java
@@ -1,16 +1,37 @@
+import static java.lang.Integer.parseInt;
+
 public class PostfixCalc implements IPostfixCalc {
 
     StackArrayList Stack = new StackArrayList();
 
     @Override
     public int Evaluate(String expresion) {
-
+        int valor1;
+        int valor2;
+        String operando;
+        int resultado;
         String[] elementos = expresion.split(" ");
-        if (Stack.isEmpty()){
-            Stack.push(elementos);
+        for (String elemento: elementos){
+            while (Stack.count()!=2){
+                if (elemento.equals("1") | elemento.equals("2") | elemento.equals("3") || elemento.equals("4") || elemento.equals("5") | elemento.equals("6") | elemento.equals("7") | elemento.equals("8") | elemento.equals("9")){
+                    Stack.push(parseInt(elemento));
+                }
+            }
+            if (elemento.equals("+") | elemento.equals("-") | elemento.equals("/") || elemento.equals("*")){
+                Stack.push(elemento);
+            }
+            valor1 = (int) Stack.peek();
+            Stack.pull();
+            valor2 = (int) Stack.peek();
+            Stack.pull();
+            operando = (String) Stack.peek();
+            if (operando== "+"){
+                resultado = valor1 + valor2;
+            } else if (operando == "-"){
+                resultado = valor1 - valor2;
+            }
 
         }
-
         return 0;
     }
 }
